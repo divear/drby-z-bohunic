5efe7ae698c4d2642f3fdccb30d4eb8c123d0250
lukascobit <lukyn.odehnal333@gmail.com>
2022-02-03 13:31:20 +0100

    post request to the backend

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function NotFound() {
     return <div>
         <h1>stránka nenelezena  </h1>
+        <title>stránka nenelezena</title>
     </div>;
 }
 
--- src/components/NovaZprava.jsx
+++ src/components/NovaZprava.jsx
@@ -1,15 +1,44 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function NovaZprava() {
+    const serverDomain = process.env.REACT_APP_SERVERDOMAIN
     const [username, setUsername] = useState("")
     const [body, setBody] = useState("")
+    const [errorCode, setErrorCode] = useState("");
 
-    //todo: connect this to the backend
+    async function submit(e) {
+        e.preventDefault()
+        if (!body | !username) {
+            setErrorCode("Všechna pole jsou povinná!");
+            return
+        }
+        localStorage.setItem("username", username)
+
+        const Rbody = { body }
+        const Rusername = { username }
+
+        const arr = [Rbody, Rusername]
+
+        console.log(arr[1].username);
+
+        const response = await fetch(`${serverDomain}drby`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(arr)
+        })
+        console.log(response);
+        window.location = "/"
+
+    }
+
+
+
 
     return <div>
-        <h1>Nová zpráva</h1>
+        <title>Nová zpráva</title>
 
-        <form action="" className='novaZprava'>
+        <form onSubmit={(e) => submit(e)} action="" className='novaZprava'>
             <label htmlFor="username">Přezdívka <i>(nepovinná):</i></label><br />
             <input value={username} onChange={e => setUsername(e.target.value)} type="text" className='username' id='username' placeholder='Např. b0br123' />
 
@@ -17,6 +46,8 @@ function NovaZprava() {
                 <label htmlFor="body">Zpráva</label><br />
                 <textarea value={body} onChange={e => setBody(e.target.value)} name="body" id="body" placeholder='Např. želvy jsou overrated'></textarea>
             </div>
+            <button>Poslat</button>
+            <h2>{errorCode}</h2>
         </form>
     </div>;
 }
