38ae26ed09dcf1e72a6e0e88182641d9685b8509
lukascobit <lukyn.odehnal333@gmail.com>
2022-02-07 22:25:16 +0100

    feedback page

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Feedback() {
+    return <div>
+        <h1>Feedback</h1>
+        <title>Feedback</title>
+        <h3>Jestli jsi našel/la nějaký bugy či co, tak mi napiš:</h3>
+
+        <a target="_blank" href="https://www.instagram.com/lukas.ode/">Instagram</a>
+    </div>;
+}
+
+export default Feedback;
